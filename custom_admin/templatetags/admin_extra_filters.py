5620a0a4999a13c74998f4b62b72825b32ef4553
mobidev86 <mobidev86@gmail.com>
2022-12-14 19:32:22 +0530

    Remove print

--- custom_admin/templatetags/admin_extra_filters.py
+++ custom_admin/templatetags/admin_extra_filters.py
@@ -42,5 +42,4 @@ def generate_og_image(text_data, slug=None, type=None):
 @register.simple_tag
 def set_bg_color(code):
     color_code = f'bg-[{code}]' if code else 'bg-[#d42020]'
-    print(color_code,"color_code")
     return color_code
